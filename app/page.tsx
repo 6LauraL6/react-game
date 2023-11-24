d7b559434d0a456f59926457534b1280082e3fd8
Laura Lopez <lauralopezespadal6@gmail.com>
2023-11-24 18:14:36 +0000

    CLICAR X .

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,37 @@
 import Image from 'next/image'
 
+'use client';
+import { useState } from 'react';
+
 export default function Home() {
   return (
     <div className='container m-2'>
       <div className='row'>
-        <Square value="1"/>
-        <Square value="2"/>
-        <Square value="3"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className='row'>
-        <Square value="4"/>
-        <Square value="5"/>
-        <Square value="6"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className='row'>
-        <Square value="7"/>
-        <Square value="8"/>
-        <Square value="9"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
     </div>
     
   )
-  function Square({value}){
-    return <div className='col-3 col-md-2 p-3 border fs-1 fw-bold text-center'>{value}</div>
+  function Square(){
+
+    const [value,setValue]=useState('.')
+    
+    function handleClick(){
+      setValue('X');
+    }
+    return <div onClick={handleClick} className='col-3 col-md-2 p-3 border 
+    fs-1 fw-bold text-center'>{value}</div>
   }
 }
