4a35ea0983563f7705bb29afe61d1a9929f151cf
Laura Lopez <lauralopezespadal6@gmail.com>
2023-11-24 17:48:26 +0000

    tabla

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,22 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='container mt-5'>
-      <button className='p-3 fs-1 fw-bold'>X</button>
+    <div className='container m-2'>
+      <div className='row'>
+        <div className='col-3 col-md-2 p-3 border fs-1 fw-bold text-center'>1</div>
+        <div className='col-3 col-md-2 p-3 border fs-1 fw-bold text-center'>2</div>
+        <div className='col-3 col-md-2 p-3 border fs-1 fw-bold text-center'>3</div>
+      </div>
+      <div className='row'>
+        <div className='col-3 col-md-2 p-3 border fs-1 fw-bold text-center'>4</div>
+        <div className='col-3 col-md-2 p-3 border fs-1 fw-bold text-center'>5</div>
+        <div className='col-3 col-md-2 p-3 border fs-1 fw-bold text-center'>6</div>
+      </div>
+      <div className='row'>
+        <div className='col-3 col-md-2 p-3 border fs-1 fw-bold text-center'>7</div>
+        <div className='col-3 col-md-2 p-3 border fs-1 fw-bold text-center'>8</div>
+        <div className='col-3 col-md-2 p-3 border fs-1 fw-bold text-center'>9</div>
+      </div>
     </div>
   )
 }
