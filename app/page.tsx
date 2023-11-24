9301e08e8a235029da3265304017765c561b1bf2
Laura Lopez <lauralopezespadal6@gmail.com>
2023-11-24 17:55:20 +0000

    Square

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,26 @@ export default function Home() {
   return (
     <div className='container m-2'>
       <div className='row'>
-        <div className='col-3 col-md-2 p-3 border fs-1 fw-bold text-center'>1</div>
-        <div className='col-3 col-md-2 p-3 border fs-1 fw-bold text-center'>2</div>
-        <div className='col-3 col-md-2 p-3 border fs-1 fw-bold text-center'>3</div>
+        <Square/>
+        <Square/>
+        <Square/>
       </div>
       <div className='row'>
-        <div className='col-3 col-md-2 p-3 border fs-1 fw-bold text-center'>4</div>
-        <div className='col-3 col-md-2 p-3 border fs-1 fw-bold text-center'>5</div>
-        <div className='col-3 col-md-2 p-3 border fs-1 fw-bold text-center'>6</div>
+        <Square/>
+        <Square/>
+        <Square/>
       </div>
       <div className='row'>
-        <div className='col-3 col-md-2 p-3 border fs-1 fw-bold text-center'>7</div>
-        <div className='col-3 col-md-2 p-3 border fs-1 fw-bold text-center'>8</div>
-        <div className='col-3 col-md-2 p-3 border fs-1 fw-bold text-center'>9</div>
+        <Square/>
+        <Square/>
+        <Square/>
       </div>
     </div>
+    
   )
+  function Square(){
+    return <div className='col-3 col-md-2 p-3 border fs-1 fw-bold text-center'>1</div>
+
+
+  }
 }
