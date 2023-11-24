da1b014f57751051b30e5c8b4078735da6e44a3b
Laura Lopez <lauralopezespadal6@gmail.com>
2023-11-24 18:00:49 +0000

    cuadrícula de números

--- app/page.tsx
+++ app/page.tsx
@@ -4,26 +4,24 @@ export default function Home() {
   return (
     <div className='container m-2'>
       <div className='row'>
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square value="1"/>
+        <Square value="2"/>
+        <Square value="3"/>
       </div>
       <div className='row'>
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square value="4"/>
+        <Square value="5"/>
+        <Square value="6"/>
       </div>
       <div className='row'>
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square value="7"/>
+        <Square value="8"/>
+        <Square value="9"/>
       </div>
     </div>
     
   )
-  function Square(){
-    return <div className='col-3 col-md-2 p-3 border fs-1 fw-bold text-center'>1</div>
-
-
+  function Square({value}){
+    return <div className='col-3 col-md-2 p-3 border fs-1 fw-bold text-center'>{value}</div>
   }
 }
